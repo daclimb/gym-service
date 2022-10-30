24ace91254e302e21ca0e647797f8e12bdeac46c
jonghoon <>
2022-10-30 23:23:13 +0900

    github action 오류 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     kotlin("plugin.jpa") version "1.6.21"
 }
 
-group = "app.joybox"
+group = "app.daclimb"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
